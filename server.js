bf0071cd5ce58a5f324475f57aaccd3987f7e1d6
Dave Baskind <dave@picketstudio.com>
2020-08-14 08:07:46 +1000

    Move Glitch/Git webhook to own Router

--- server.js
+++ server.js
@@ -7,42 +7,6 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// Webhook for updating Glitch via a Github adapted from
-// https://github.com/nmcardoso/glitch-github-sync/
-const cmd = require('node-cmd');
-const crypto = require('crypto');
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
-const verifySignature = (req, res, next) => {
-  const payload = JSON.stringify(req.body);
-  const hmac = crypto.createHmac('sha1', process.env.GITHUB_SECRET);
-  const digest = 'sha1=' + hmac.update(payload).digest('hex');
-  const checksum = req.headers['x-hub-signature'];
-
-  if (!checksum || !digest || checksum !== digest) {
-    return res.status(403).send('auth failed');
-  }
-  return next();
-
-}
-
-app.post('/git', verifySignature, (req, res) => {
-  if (req.headers['x-github-event'] == 'push') {
-    cmd.get('bash git.sh', (err, data) => {
-      if (err) return console.log(err);
-      console.log(data);
-      cmd.run('refresh');
-      return res.status(200).send(data);
-    });
-  } else if (req.headers['x-github-event'] == 'ping') {
-    return res.status(200).send('PONG');
-  } else {
-    return res.status(200).send('Unsuported Github event. Nothing done.');
-  }
-});
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -50,6 +14,10 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+// Git/Glitch syncing webhook
+const gitWebhook = require('./routers/git.js');
+app.use('/git', gitWebhook);
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
